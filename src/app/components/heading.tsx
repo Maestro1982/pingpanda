bc029d984299ff2a01fa63ac440c0695a557bec7
Maestro1982 <kris.tote@telenet.be>
2024-11-04 15:15:09 +0100

    implement landing page and reusable components

---
+++ src/app/components/heading.tsx
@@ -0,0 +1,21 @@
+import { HTMLAttributes } from "react"
+
+import { cn } from "@/utils"
+
+interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  children?: React.ReactNode
+}
+
+export const Heading = ({ children, className, ...props }: HeadingProps) => {
+  return (
+    <h1
+      className={cn(
+        "text-4xl sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zinc-800",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </h1>
+  )
+}
