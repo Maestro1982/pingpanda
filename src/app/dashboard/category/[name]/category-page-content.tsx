9ae71cae1c498847a1daf3f9be4e6df76ce5dbe4
Maestro1982 <kris.tote@telenet.be>
2024-11-21 14:05:44 +0100

    continue working on event details page

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { useSearchParams } from "next/navigation"
+import { BarChart } from "lucide-react"
+import { isAfter, isToday, startOfMonth, startOfWeek } from "date-fns"
 import { EventCategory } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 
+import { client } from "@/lib/client"
+
 import { CategoryEmptyState } from "@/app/dashboard/category/[name]/category-empty-state"
 
-import { client } from "@/lib/client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card } from "@/components/ui/card"
 
 interface CategoryPageContentProps {
   hasEvents: boolean
@@ -61,7 +67,136 @@ export const CategoryPageContent = ({
     ],
   })
 
+  /* useMemo to not re-calculate when the page refreshes, ONLY re-calculate when data?.events changes */
+  const numericFieldSums = useMemo(() => {
+    if (!data?.events || data.events.length === 0) return {}
+
+    const sums: Record<
+      string,
+      {
+        total: number
+        thisWeek: number
+        thisMonth: number
+        today: number
+      }
+    > = {}
+
+    const now = new Date()
+    const startWeek = startOfWeek(now, { weekStartsOn: 0 })
+    const startMonth = startOfMonth(now)
+
+    data.events.forEach((event) => {
+      const eventDate = event.createdAt
+
+      Object.entries(event.fields as object).forEach(([field, value]) => {
+        if (typeof value === "number") {
+          if (!sums[field]) {
+            sums[field] = { total: 0, thisWeek: 0, thisMonth: 0, today: 0 }
+          }
+
+          sums[field].total += value
+
+          if (
+            isAfter(eventDate, startWeek) ||
+            eventDate.getTime() === startWeek.getTime()
+          ) {
+            sums[field].thisWeek += value
+          }
+
+          if (
+            isAfter(eventDate, startMonth) ||
+            eventDate.getTime() === startMonth.getTime()
+          ) {
+            sums[field].thisMonth += value
+          }
+
+          if (isToday(eventDate)) {
+            sums[field].today += value
+          }
+        }
+      })
+    })
+    return sums
+  }, [data?.events])
+
+  const NumericFieldSumCards = () => {
+    if (Object.keys(numericFieldSums).length === 0) return null
+
+    return Object.entries(numericFieldSums).map(([field, sums]) => {
+      const relevantSum =
+        activeTab === "today"
+          ? sums.today
+          : activeTab === "week"
+          ? sums.thisWeek
+          : sums.thisMonth
+
+      return (
+        <Card key={field}>
+          <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <p className="text-sm/6 font-medium">
+              {field.charAt(0).toUpperCase() + field.slice(1)}
+            </p>
+            <BarChart className="size-4 text-muted-foreground" />
+          </div>
+
+          <div>
+            <p className="text-2xl font-bold">{data?.eventsCount || 0}</p>
+            <p className="text-xs/5 text-muted-foreground">
+              {activeTab === "today"
+                ? "today"
+                : activeTab === "week"
+                ? "this week"
+                : "this month"}
+            </p>
+          </div>
+        </Card>
+      )
+    })
+  }
+
   if (!pollingData.hasEvents) {
     return <CategoryEmptyState categoryName={category.name} />
   }
+
+  return (
+    <div className="space-y-6">
+      <Tabs
+        value={activeTab}
+        onValueChange={(value) => {
+          setActiveTab(value as "today" | "week" | "month")
+        }}
+      >
+        <TabsList className="mb-2">
+          <TabsTrigger value="today">Today</TabsTrigger>
+          <TabsTrigger value="week">This Week</TabsTrigger>
+          <TabsTrigger value="month">This Month</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value={activeTab}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
+            <Card className="border-2 border-brand-700">
+              <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <p className="text-sm/6 font-medium">Total Events</p>
+                <BarChart className="size-4 text-muted-foreground" />
+              </div>
+
+              <div>
+                <p className="text-2xl font-bold">{data?.eventsCount || 0}</p>
+                <p className="text-xs/5 text-muted-foreground">
+                  {(data?.eventsCount ?? 0) === 1 ? "Event" : "Events"}{" "}
+                  {activeTab === "today"
+                    ? "today"
+                    : activeTab === "week"
+                    ? "this week"
+                    : "this month"}
+                </p>
+              </div>
+            </Card>
+
+            <NumericFieldSumCards />
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
 }
