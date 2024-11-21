199e614a33b9bddf87f5da4178461137c82451f2
Maestro1982 <kris.tote@telenet.be>
2024-11-21 11:58:53 +0100

    continue working on event details page

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { useState } from "react"
+import { useSearchParams } from "next/navigation"
 import { EventCategory } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 
 import { CategoryEmptyState } from "@/app/dashboard/category/[name]/category-empty-state"
+
+import { client } from "@/lib/client"
 
 interface CategoryPageContentProps {
   hasEvents: boolean
@@ -14,9 +18,47 @@ export const CategoryPageContent = ({
   hasEvents: initialHasEvents,
   category,
 }: CategoryPageContentProps) => {
+  const searchParams = useSearchParams()
+
+  const [activeTab, setActiveTab] = useState<"today" | "week" | "month">(
+    "today"
+  )
+
+  /* example: http://localhost:3000/dashboard/category/sale?page=1&limit=30 */
+  const page = parseInt(searchParams.get("page") || "1", 10)
+
+  const limit = parseInt(searchParams.get("limit") || "30", 10)
+
+  const [pagination, setPagination] = useState({
+    pageIndex: page - 1,
+    pageSize: limit,
+  })
+
   const { data: pollingData } = useQuery({
     queryKey: ["category", category.name, "hasEvents"],
     initialData: { hasEvents: initialHasEvents },
+  })
+
+  const { data, isFetching } = useQuery({
+    queryFn: async () => {
+      const res = await client.category.getEventsByCategoryName.$get({
+        name: category.name,
+        page: pagination.pageIndex + 1,
+        limit: pagination.pageSize,
+        timeRange: activeTab,
+      })
+
+      return await res.json()
+    },
+    refetchOnWindowFocus: false,
+    enabled: pollingData.hasEvents,
+    queryKey: [
+      "events",
+      category.name,
+      pagination.pageIndex,
+      pagination.pageSize,
+      activeTab,
+    ],
   })
 
   if (!pollingData.hasEvents) {
